ac37a99f12d5afa96938da7df0059861a60344b0
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-11-01 15:36:12 -0300

    Last Changes

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
-import { setupCounter } from './counter.js'
+console.log(1)
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
+const contar = async() => {
+   console.log(2)
+   console.log(await Promise.resolve(3))
+}
+contar()
 
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+console.log(4)
